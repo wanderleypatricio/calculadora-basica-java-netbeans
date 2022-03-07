9d1c334959282a72ffe25405ef8b9cbbe1b4fbfb
Wanderley Patrício <wpatricio26@gmail.com>
2022-03-07 14:16:19 -0300

    Criando projeto calculadora

---
+++ calculadora-basica-java-netbeans/src/calculadora/basica/java/netbeans/CalculadoraBasicaJavaNetbeans.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora.basica.java.netbeans;
+
+/**
+ *
+ * @author Wanderley Patrício
+ */
+public class CalculadoraBasicaJavaNetbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculadoraView cv = new CalculadoraView();
+        cv.show();
+    }
+    
+}
